2b54a33416eb4172ceaf62923f07c158266f70c1
zvonimirbrisevac <zb51687@fer.hr>
2021-07-11 19:44:52 +0200

    added anotations v1.0

--- spring_core/build.gradle.kts
+++ spring_core/build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.10"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.5.20"
+
 }
 
 group = "com.infinum.academy"
@@ -13,6 +15,12 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+
+    implementation("org.springframework:spring-context:5.3.7")
+    testImplementation("org.springframework:spring-test:5.3.7")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
+    testImplementation("org.assertj:assertj-core:3.20.2")
+    testImplementation("io.mockk:mockk:1.12.0")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ spring_core/src/main/kotlin/Homework_02.kt
@@ -0,0 +1,106 @@
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+import org.springframework.core.io.Resource
+import org.springframework.stereotype.Component
+import java.io.FileOutputStream
+
+@Configuration
+@ComponentScan
+@PropertySource("classpath:application.properties")
+class ApplicationConfiguration
+
+interface CourseRepository {
+    fun insert(name: String): Long
+    fun findById(id: Long): Course
+    fun deleteById(id: Long): Course
+}
+
+data class Course(
+    val id: Long,
+    val name: String
+)
+
+@Component
+class InMemoryCourseRepository (dataSource: DataSource): CourseRepository {
+    private val courses = mutableMapOf<Long, Course>()
+
+    init {
+        println(dataSource)
+    }
+    override fun insert(name: String): Long {
+        val id = (courses.keys.maxOrNull() ?: 0) + 1
+        courses[id] = Course(id, name)
+        return id
+    }
+    override fun findById(id: Long): Course {
+        return courses[id] ?: throw CourseNotFoundException(id)
+    }
+    override fun deleteById(id: Long): Course {
+        return courses.remove(id) ?: throw CourseNotFoundException(id)
+    }
+}
+class CourseNotFoundException(id: Long): RuntimeException("Course with and ID $id not found")
+
+@Component
+class InFileCourseRepository(
+    @Value("\${database.name}") private val coursesFileResource: Resource // will be provided through dependency
+) : CourseRepository {
+    init {
+        if (coursesFileResource.exists().not()) {
+            coursesFileResource.file.createNewFile()
+        }
+    }
+    override fun insert(name: String): Long {
+        val file = coursesFileResource.file
+        val id = (file.readLines()
+            .filter { it.isNotEmpty() }
+            .map { line -> line.split(",").first().toLong() }
+            .maxOrNull() ?: 0) + 1
+        file.appendText("$id,$name\n")
+        return id
+    }
+    override fun findById(id: Long): Course {
+        return coursesFileResource.file.readLines()
+            .filter { it.isNotEmpty() }
+            .find { line -> line.split(",").first().toLong() == id }
+            ?.let { line ->
+                val tokens = line.split(",")
+                Course(id = tokens[0].toLong(), name = tokens[1])
+            }
+            ?: throw CourseNotFoundException(id)
+    }
+    override fun deleteById(id: Long): Course {
+        val coursesLines = coursesFileResource.file.readLines()
+        var lineToDelete: String? = null
+        FileOutputStream(coursesFileResource.file)
+            .writer()
+            .use { fileOutputWriter ->
+                coursesLines.forEach { line ->
+                    if (line.split(",").first().toLong() == id) {
+                        lineToDelete = line
+                    } else {
+                        fileOutputWriter.appendLine(line)
+                    }
+                }
+            }
+        return lineToDelete?.let { line ->
+            val tokens = line.split(",")
+            Course(id = tokens[0].toLong(), name = tokens[1])
+        } ?: throw CourseNotFoundException(id)
+    }
+}
+
+@Component
+data class DataSource(
+    @Value("\${database.name}") val dbName: String,
+    @Value("\${database.username}") val username: String,
+    @Value("\${database.password}") val password: String
+)
+
+fun main() {
+    val appConfig: ApplicationContext= AnnotationConfigApplicationContext { ApplicationConfiguration::class.java }
+}
